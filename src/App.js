c53013fd66485be8b0d2d94c9f4d8a66885bf202
WeslleiBrito <wesllei.wbs@gmail.com>
2023-04-19 21:01:37 -0300

    Parte 1 finalizada

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { ChakraProvider, Heading } from "@chakra-ui/react";
+import Card from "./components/Card";
 
 export default function App() {
   const [users, setUsers] = useState([]);
@@ -22,9 +24,12 @@ export default function App() {
   };
 
   return (
-    <>
-      <h1>Me apague quando for iniciar!</h1>
-      <p>Chame o Card aqui!</p>
-    </>
+    <ChakraProvider>
+      {users.map((user) => {
+        return (
+          <Card key={user.id} user={user}/>
+        )
+      })}
+    </ChakraProvider>
   );
 }
